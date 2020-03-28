014ff6fba92256b2e3b466950770c034f35877a6
grmohanram <gr.mohanram@yahoo.com>
2020-03-28 11:19:03 +0530

    code change

--- src/GitCICD.java
+++ src/GitCICD.java
@@ -4,7 +4,7 @@ public class GitCICD {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Git Hub Testing  ");
+		System.out.println("Git Hub Testing   ");
 
 	}
 
